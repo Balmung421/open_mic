58d2f85bbf969c515f577fa78691daae8ba07702
Balmung421 <zcook421@gmail.com>
2017-07-19 09:50:01 -0600

    Instance of joke that can hold info

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Joke
+
+  def initialize(jokes = {})
+    # jokes[:id]
+    # jokes[:question]
+    # jokes[:answer]
+    @jokes = jokes
+
+    # jokes = {id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."}
+  end
+  # binding.pry
+
+  def jokes
+    # @jokes = Jokes.new({})
+    @jokes = {id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."}
+  end
+
+  binding.pry
 
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -2,7 +2,19 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/joke'
+require 'pry'
 
 class JokeTest < Minitest::Test
 
+  def test_if_it_exists
+    joke = Joke.new
+    assert_instance_of Joke, joke
+  end
+  # binding.pry
+
+  def test_if_it_can_hold_stuff
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    jokes = {id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."}
+    assert_equal jokes, joke.jokes
+  end
 end
